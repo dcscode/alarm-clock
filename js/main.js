b049635792bd78c7d46fac4fbc363fcf148c274b
Delaney Cifranik-Stovall <delaneycscode@gmail.com>
2022-02-10 18:55:13 +0000

    basic date info pulled and displayed

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
 //Tip: We will be using the Date object and setInterval and/or setTimeout
 
+let timeDisplay = document.querySelector('time');
+let timeContainer = document.getElementById('container');
+
+let currentTime = new Date();
+let hour = currentTime.getHours();
+let minutes = currentTime.getMinutes();
+let seconds = currentTime.getSeconds();
+let month = currentTime.getMonth();
+let day = currentTime.getDate();
+let year = currentTime.getFullYear();
+
+let timeFromObject = year + "-" + month + "-" + day + " " + hour + ":" + minutes + ":" + seconds;
+timeDisplay.textContent = timeFromObject;
+timeDisplay.setAttribute("datetime", timeFromObject);
+timeContainer.appendChild(timeDisplay);
